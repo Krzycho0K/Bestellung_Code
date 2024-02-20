266cd6d9fd28952ed749e9845f02af6aee87160c
Krzycho0K <159929814+Krzycho0K@users.noreply.github.com>
2024-02-20 15:01:21 +0000

    update

--- com/cc/java/ArrayPlayground.java
+++ com/cc/java/ArrayPlayground.java
@@ -1,23 +1,48 @@
 package com.cc.java;
 
-
 public class ArrayPlayground {
 
-	static int [] numArr = {2,4,6,7};
+	static int [] numArr = {2,4,6,7,20};
 	// static String strArr = {"Ich", "bin", "Maxine", "Mützerich"};
 
     public static void main(String[] args) {
 
-		System.out.println(numArr);	
-		System.out.println("Element 1: " + numArr[0]);
-		System.out.println("Element 2: " + numArr[1]);
-		System.out.println("Element 3: " + numArr[2]);
-		System.out.println("Element 4: " + numArr[3]);
+		// System.out.println(numArr);
+
+		System.out.println("Alle Aray Elemente: " + numArr.length + " {" + numArr[0] + "," + numArr[1] + "," + numArr[2] + "," + numArr[3] + "," + numArr[4] + "}");
+		
+		outputArr();
+
+		numArr[0] = 100;
+		System.out.println("Erste Array Element auf 100 setzen ");
+		
+		// numArr[4] = 101
+		numArr[numArr.length-1] = 101;
+		System.out.println("Letzte Array Element auf 101 setzen ");
+
+		outputArr();
 		
 		System.out.println(" ");
-		System.out.println("Summe: " + numArr[0] + "+" + numArr[1] + "+" + numArr[2] + "+" + numArr[3] + " = " + (numArr[0] + numArr[1] + numArr[2] + numArr[3]));
-		
-
+		System.out.println("Summe: " + numArr[0] + "+" + numArr[1] + "+" + numArr[2] + "+" + numArr[3] + "+" + numArr[4] + " = " + (numArr[0] + numArr[1] + numArr[2] + numArr[3] + numArr[4]));
 	}
 
+	static void outputArr(){
+		System.out.println("-----------------");
+
+		// System.out.println("Element 1: " + numArr[0]);
+		// System.out.println("Element 2: " + numArr[1]);
+		// System.out.println("Element 3: " + numArr[2]);
+		// System.out.println("Element 4: " + numArr[3]);
+		// System.out.println("Element 5: " + numArr[4]);
+
+for (int i = 0; i < numArr.length; i++)
+		if (i < numArr.length-1) {
+	System.out.println("Element " + (i+1) + " " + numArr[i]);
+		} else {
+	System.out.println("Element " + (i+1) + " " + numArr[i]);
+		}
+
+		// System.out.println("-----------------");
+	}
+	
 }
